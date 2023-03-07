5db09825c88739a419e2d128e47e31d503183f04
Ömer Tufan Ayhan <omertufan2000@hotmail.com>
2023-03-08 00:49:59 +0300

    signalr created with razor pages

---
+++ Server/Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
+        public override async Task OnConnectedAsync()
+        {
+            string username = Context.GetHttpContext().Request.Query["username"];
+            Users.Add(Context.ConnectionId, username);
+            await SendMessage(string.Empty, $"{username} gruba katıldı!");
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            string username = Users.FirstOrDefault(e => e.Key == Context.ConnectionId).Value;
+            await SendMessage(string.Empty, $"{username} gruptan ayrıldı!");
+         
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
